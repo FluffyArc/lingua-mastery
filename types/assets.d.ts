d3334dc116ce7a7fc4a019bbc5eeae4cc433f10c
FluffyArc <ferdian.aditya2302@gmail.com>
2026-06-29 08:24:35 +0700

    implement the onboarding screen

--- types/assets.d.ts
+++ types/assets.d.ts
@@ -2,3 +2,8 @@ declare module "*.ttf" {
   const font: import("expo-font").FontSource;
   export default font;
 }
+
+declare module "*.png" {
+  const image: import("react-native").ImageSourcePropType;
+  export default image;
+}
